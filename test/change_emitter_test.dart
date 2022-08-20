296e37d36d790c0813d3d2e1fc0f8d40ec8acdee
Jonathan Aird <jonathan.aird@gmail.com>
2022-08-20 09:41:28 +0200

    organizing tests

--- test/change_emitter_test.dart
+++ test/change_emitter_test.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'dart:async';
+// import 'dart:math';
 
 import 'package:flutter_test/flutter_test.dart';
 
@@ -16,41 +17,66 @@ class ExampleEmitter extends EmitterContainer {
 
 void main() {
   //list emitter tests
-  test('ListEmitter test of removeRange', () {
-    var list = [1, 2, 3, 4, 5, 6];
-    var listEmitter = ListEmitter(list);
+  group('ListEmitter', () {
+    test('removeRange should work as expected', () {
+      var list = [1, 2, 3, 4, 5, 6];
+      var listEmitter = ListEmitter(list);
 
-    listEmitter.removeRange(1, 4);
-    list.removeRange(1, 4);
-    expect(eq.equals(list, listEmitter), true);
-  });
-
-  test('ListEmitter test of setting new length', () {
-    var list = <int?>[1, 2, 3, 4, 5, 6];
-    var listEmitter = ListEmitter(list);
-
-    listEmitter.length = 9;
-    list.length = 9;
-    expect(eq.equals(list, listEmitter), true);
-  });
-
-  test('ListEmitter multi-unit test', () {
-    var list = <int?>[1, 2, 3, 4, 5, 6];
-    var listEmitter = ListEmitter(list);
-
-    void visitList(List<int?> list) {
-      list.length = 9;
-      list.addAll([5, 3, 7, 8]);
-      list.removeLast();
-      list.retainWhere((element) => element != null);
+      listEmitter.removeRange(1, 4);
       list.removeRange(1, 4);
-      list.replaceRange(2, list.length - 1, [1, 2, 3]);
-    }
+      expect(eq.equals(list, listEmitter), true);
+    });
 
-    visitList(list);
-    visitList(listEmitter);
+    test('setting new length', () {
+      var list = <int?>[1, 2, 3, 4, 5, 6];
+      var listEmitter = ListEmitter(list);
 
-    expect(eq.equals(list, listEmitter), true);
+      listEmitter.length = 9;
+      list.length = 9;
+      expect(eq.equals(list, listEmitter), true);
+    });
+
+    test('multi-unit test', () {
+      var list = <int?>[1, 2, 3, 4, 5, 6];
+      var listEmitter = ListEmitter(list);
+
+      void visitList(List<int?> list) {
+        list.length = 9;
+        list.addAll([5, 3, 7, 8]);
+        list.removeLast();
+        list.retainWhere((element) => element != null);
+        list.removeRange(1, 4);
+        list.replaceRange(2, list.length - 1, [1, 2, 3]);
+      }
+
+      visitList(list);
+      visitList(listEmitter);
+
+      expect(eq.equals(list, listEmitter), true);
+    });
+    test('only emits once using a transaction', () async {
+      final list = ListEmitter([1, 2, 3, 4]);
+      var numEmits = 0;
+      list.changes.listen((event) => numEmits++);
+      list.startTransaction();
+      list.add(3);
+      list.remove(2);
+      list[0] = 6;
+      list.endTransaction();
+      scheduleMicrotask(() => expect(numEmits, 1));
+    });
+    test('emits once on methods that nest other mutation methods', () async {
+      final list = ListEmitter([2, 3, 4, 5]);
+      var numEmits = 0;
+      list.changes.listen((event) => numEmits++);
+      list.addAll([4, 5, 67, 3]);
+      scheduleMicrotask(() => expect(numEmits, 1));
+    });
+    test('check for concurrent modification during iteration of removewhere', () {
+      var list = ListEmitter([1, 2, 3, 4]);
+      list.removeWhere((element) => element == 1);
+      expect(eq.equals(list, [2, 3, 4]), true);
+    });
   });
 
   test('MapEmitter multi-unit test', () {
@@ -66,13 +92,6 @@ void main() {
     visitMap(mapEmitter);
 
     expect(eq.equals(map, mapEmitter), true);
-  });
-
-  //regression tests
-  test('check for concurrent modification during iteration of removewhere', () {
-    var list = ListEmitter([1, 2, 3, 4]);
-    list.removeWhere((element) => element == 1);
-    expect(eq.equals(list, [2, 3, 4]), true);
   });
 
   test('EmitterList disposes of removed children', () async {
@@ -97,18 +116,6 @@ void main() {
     expect(example, ancestor);
   });
 
-  test('transactions work on Lists', () async {
-    final list = ListEmitter([1, 2, 3, 4]);
-    var numEmits = 0;
-    list.changes.listen((event) => numEmits++);
-    list.startTransaction();
-    list.add(3);
-    list.remove(2);
-    list[0] = 6;
-    list.endTransaction();
-    await Future.delayed(Duration(milliseconds: 300));
-    expect(numEmits, 1);
-  });
   test('transactions work on maps', () async {
     final map = MapEmitter({0: 1, 4: 5, 3: 4});
     var numEmits = 0;
@@ -121,15 +128,6 @@ void main() {
     map[0] = 6;
     map.endTransaction();
     await Future.delayed(Duration(milliseconds: 500));
-    expect(numEmits, 1);
-  });
-
-  test('list emits once on methods that nest other mutation methods', () async {
-    final list = ListEmitter([2, 3, 4, 5]);
-    var numEmits = 0;
-    list.changes.listen((event) => numEmits++);
-    list.addAll([4, 5, 67, 3]);
-    await Future.delayed(Duration(milliseconds: 300));
     expect(numEmits, 1);
   });
 
